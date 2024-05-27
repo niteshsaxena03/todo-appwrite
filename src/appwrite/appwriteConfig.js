f0383523e7346afabf1b335488e9557f6efb573c
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-28 04:54:18 +0530

    Setting up database and config is fine now

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -4,4 +4,9 @@ const client=new Client();
 
 client.setEndpoint("http://localhost/v1").setProject("66540d900012c74232e4");
 
+//Account
 export const account=new Account(client);
+
+//Database
+
+export const databases = new Databases(client, "66540dcf001fc3983bb5");
