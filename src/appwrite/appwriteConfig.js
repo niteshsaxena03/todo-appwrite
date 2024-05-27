351b348299e4b4a4757615f07832a8547b2f4300
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-28 04:52:24 +0530

    Setting up client and account

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -1,1 +1,7 @@
 import { Client,Account,Databases } from "appwrite";
+
+const client=new Client();
+
+client.setEndpoint("http://localhost/v1").setProject("66540d900012c74232e4");
+
+export const account=new Account(client);
