4074ad1c167fc2a908ef454451856c32d6e59b55
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-28 05:00:21 +0530

    basic things for signup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react"
+import {account} from '../appwrite/appwriteConfig';
+import { useNavigate } from "react-router-dom";
+import {v4 as uuid} from  'uuid';
+
 
 function SignUp() {
+  const navigation=useNavigate();
+  
+  //creating basic structure of user object for signup purpose
+  const [user,setUser]=useState({
+    name:'',
+    email: '',
+    password: ''
+  })
   return (
     <div>
       SignUp
