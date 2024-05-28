40cd34b48aaaeac14fe12fcb52a94e3436723d07
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-28 05:46:30 +0530

    basic setup for login:

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,11 @@
-
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { account } from "../appwrite/appwriteConfig"
 function Login() {
+  const [user,setUser]=useState({
+    email:"",
+    password:"",
+  })
   return (
     <div>
       Login
