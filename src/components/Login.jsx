7be519c59628d2dd9dc4d8f35fe72f7d32bd62cd
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-28 05:53:06 +0530

    creating main function of login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,10 +2,23 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { account } from "../appwrite/appwriteConfig"
 function Login() {
+  const navigation = useNavigate();
   const [user,setUser]=useState({
     email:"",
     password:"",
   })
+
+  //main login function below
+  const loginUser=async (e)=>{
+    e.preventDefault()
+    try{
+      await account.createEmailPasswordSession(user.email,user.password);
+      navigation('/profile');
+    }catch(error){
+      console.log(error);
+    }
+  }
+
   return (
     <div>
       Login
