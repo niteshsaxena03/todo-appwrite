e9ebc7fd670d54743ed26bd1be458c315bc3f973
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-29 12:33:24 +0530

    Some changes in todoform

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { databases } from "../appwrite/appwriteConfig";
 
-
 function TodoForm() {
-  const [todo, setTodo]=useState("");
-  return (
-    <div>
-      TodoForm
-    </div>
-  )
+  const [todo, setTodo] = useState("");
+  return <div>TodoForm</div>;
 }
 
-export default TodoForm
+export default TodoForm;
