0ba55639391331b98483648cbc4c9622bfcf8a1c
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-29 12:32:02 +0530

    Started working on todo form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
+import { databases } from "../appwrite/appwriteConfig";
+
 
 function TodoForm() {
+  const [todo, setTodo]=useState("");
   return (
     <div>
       TodoForm
