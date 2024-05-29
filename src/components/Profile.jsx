01630c5069569e6c3a0d9e940d336eb108df6627
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-29 12:14:44 +0530

    Working on profile component and creating logout component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,67 @@
 import { account } from "../appwrite/appwriteConfig";
 import { useNavigate,Link } from "react-router-dom";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+import { useState,useEffect } from "react";
 
 function Profile() {
+  const navigate=useNavigate();
+
+  const [userDetails,setUserDetails]=useState();
+  
+
+  //all changes within useEffect hook
+  useEffect(() => {
+    const getData=account.get();
+
+    getData.then(
+      function(response){
+        setUserDetails(response);
+      },
+      function(error){
+        console.log(error);
+      }
+    )
+
+    //function to log out user
+    function handleLogOut=async()=>{
+      try{
+        await account.deleteSession("current");
+        navigate("/");
+      }catch(error){
+        console.log(error);
+      }
+    }
+  }, []);
+  
   return (
-    <div>
-      <h1>Profile page</h1>
-      <h2>Nothing written as of now</h2>
-    </div>
+    <>
+      {userDetails ? (
+        <>
+          <div className="min-h-min max-w-7xl mx-auto shadow-md flex justify-between text-right py-3 px-3 mt-2 rounded-md">
+            <div>
+              <p className="text-xl">Hello {userDetails.name}</p>
+            </div>
+            <div>
+              <button className="bg-red-400 text-white p-1 rounded-md" onClick={handleLogOut}>
+                Logout
+              </button>
+            </div>
+          </div>
+          <TodoForm />
+          <Todo />
+        </>
+      ) : (
+        <p className="mt-4">
+          Please Login To see Profile{" "}
+          <Link to="/">
+            <span className="bg-blue-300 p-2 cursor-pointer text-white rounded-md">
+              Login
+            </span>
+          </Link>
+        </p>
+      )}
+    </>
   );
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,30 +1,6 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"
-import { account } from "../appwrite/appwriteConfig";
+
 
 function Todo() {
-  const navigate=useNavigate();
-
-  const [userDetails,setUserDetails]=useState();
-  
-
-  //all changes within useEffect hook
-  useEffect(() => {
-    const getData=account.get();
-
-    getData.then(
-      function(response){
-        setUserDetails(response);
-      },
-      function(error){
-        console.log(error);
-      }
-    )
-  
-    return () => {
-      second
-    }
-  }, [third])
   
   return (
     <div>
