9b96b1cee6eec5dd825c615d0e46e6d6ebe51ed2
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-29 11:04:27 +0530

    error resolved

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,23 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { account } from "../appwrite/appwriteConfig"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { account } from "../appwrite/appwriteConfig";
 function Login() {
   const navigation = useNavigate();
-  const [user,setUser]=useState({
-    email:"",
-    password:"",
-  })
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-  //main login function below
-  const loginUser=async (e)=>{
-    e.preventDefault()
-    try{
-      await account.createEmailPasswordSession(user.email,user.password);
-      navigation('/profile');
-    }catch(error){
+  // main login function below
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      await account.createEmailPasswordSession(user.email, user.password);
+      navigation("/profile");
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
     <>
@@ -178,4 +178,4 @@ function Login() {
   );
 }
 
-export default Login
+export default Login;
