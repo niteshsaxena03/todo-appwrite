7d66f671f2c47b1c6fe674ff1048c11d576a1392
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-05-30 08:29:37 +0530

    Writing function to handle submission in todo form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,7 +4,24 @@ import { databases } from "../appwrite/appwriteConfig";
 
 function TodoForm() {
   const [todo, setTodo] = useState("");
-  return <div>TodoForm</div>;
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const promise = databases.createDocument("66540de80019205fb8f5",uuidv4,{todo});
+
+    promise.then(
+      function(response){
+        console.log(response);
+      },
+      function(error){
+        console.log(error);
+      }
+    )
+    window.location.reload();
+  }
+  return (
+  <div>TodoForm</div>
+  )
 }
 
 export default TodoForm;
